1c8e081010da5f933e6489d22eb5b92ab8a2a75f
João Paulo <joaopaulogomesdasilva99@gmail.com>
2022-02-01 09:33:07 -0300

    ok

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocide do seu carro é ${vel}km/h`)
+if (vel > 60){ //condição simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'china'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('voce é estranjeiro')
+}
